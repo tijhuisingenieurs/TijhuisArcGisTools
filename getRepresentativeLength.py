8df4fb900ecc027caaa017845546b78d1c5d4337
elinevanelburg <33683617+elinevanelburg@users.noreply.github.com>
2018-06-05 14:24:26 +0200

    Revert "Revert "Revert "Merge branch 'master' into uitpeilingentool"""
    This reverts commit 7d870ed4868d723d74a64e4469239ca626552bf5.

--- getRepresentativeLength.py
+++ getRepresentativeLength.py
@@ -9,7 +9,6 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'extern
 
 from gistools.utils.collection import MemCollection, OrderedDict
 from gistools.tools.representative_length import representative_length
-import random
 
 logging.basicConfig(level=logging.INFO)
 setup_logging(arcpy)
@@ -23,24 +22,12 @@ log.setLevel(logging.INFO)
 
 input_lines = arcpy.GetParameter(0)
 input_profiles = arcpy.GetParameter(1)
-id_field = arcpy.GetParameterAsText(2)
-output_file = arcpy.GetParameterAsText(3)
-output_split = arcpy.GetParameter(4)
-output_split_file = arcpy.GetParameterAsText(5)
-
-# random_nr = random.randint(0,100)
-# input_lines = "C:\Users\eline\Documents\Algemeen\GIS\Tooltesting\TestData\\representative_length\lines_2.shp"
-# input_profiles = "C:\Users\eline\Documents\Algemeen\GIS\Tooltesting\TestData\\representative_length\dwp_2.shp"
-# output_file = "C:\Users\eline\Documents\Algemeen\GIS\Tooltesting\TestData\\representative_length\Test_splitLines{0}.shp".format(
-#     random_nr
-# )
-# id_field = "prof_nr"
+output_file = arcpy.GetParameterAsText(2)
 
 # Print ontvangen input naar console
 log.info('Ontvangen parameters:')
 log.info('Hydrovakkenbestand = %s', str(input_lines))
 log.info('Profiellijnenbestand = %s', str(input_profiles))
-log.info('Veld met unieke profielcode = %s', id_field)
 log.info('Doelbestand = %s', str(output_file))
 
 log.info('Bezig met inlezen data...')
@@ -72,7 +59,6 @@ records = []
 line_cursor = arcpy.SearchCursor(input_profiles)
 fields_prof = arcpy.ListFields(input_profiles)
 point = arcpy.Point()
-prof_id_field = None
 
 for row in line_cursor:
     geom = row.getValue('SHAPE')
@@ -80,8 +66,6 @@ for row in line_cursor:
     for field in fields_prof:
         if field.baseName.lower() != 'shape':
             properties[field.baseName] = row.getValue(field.baseName)
-        if field.name == id_field:
-            prof_id_field = field
 
     records.append({'geometry': {'type': 'MultiLineString',
                                  'coordinates': [[(point.X, point.Y) for
@@ -90,10 +74,11 @@ for row in line_cursor:
 
 profile_col.writerecords(records)
 
+
 # Call tool
 log.info('Bezig met berekenen van de representatieve lengtes...')
 
-rep_length_col, split_lines_col = representative_length(line_col, profile_col, id_field)
+rep_length_col = representative_length(line_col, profile_col)
 
 # Write result to new file
 log.info('Bezig met genereren van het doelbestand...')
@@ -104,9 +89,11 @@ output_dir = os.path.dirname(output_file)
 output_fl = arcpy.CreateFeatureclass_management(output_dir, output_name, 'POLYLINE',
                                                 spatial_reference=spatial_reference)
 
+fields_repLength = next(rep_length_col.filter())['properties'].keys()
+
 # Add the fields from the profile collection to the new shapefile (excluding ID and geometry fields)
 for field in fields_prof:
-    if field.editable and field.baseName.lower() not in ['shape', 'id', 'fid', 'objectid']:
+    if field.editable and field.baseName.lower() not in ['shape', 'id', 'fid']:
         arcpy.AddField_management(output_fl, field.name, field.type, field.precision, field.scale,
                                   field.length, field.aliasName, field.isNullable, field.required,
                                   field.domain)
@@ -141,51 +128,6 @@ for l in rep_length_col.filter():
     row.setValue('tot_leng', l['properties'].get('tot_leng', None))
 
     dataset.insertRow(row)
-
-if output_split:
-    # Write resulting split lines to file
-    spatial_reference = arcpy.Describe(input_lines).spatialReference
-
-    output_name_split = os.path.basename(output_split_file).split('.')[0]
-    output_dir_split = os.path.dirname(output_split_file)
-    output_fl_split = arcpy.CreateFeatureclass_management(output_dir_split, output_name_split, 'POLYLINE',
-                                                    spatial_reference=spatial_reference)
-
-    for field in fields_line:
-        if field.editable and field.baseName.lower() not in ['shape', 'id', 'fid', 'objectid']:
-            arcpy.AddField_management(output_fl_split, field.name, field.type, field.precision, field.scale,
-                                      field.length, field.aliasName, field.isNullable, field.required,
-                                      field.domain)
-
-    arcpy.AddField_management(output_fl_split, prof_id_field.name, prof_id_field.type)
-    arcpy.AddField_management(output_fl_split, 'tot_leng', 'DOUBLE')
-
-    # Start filling the shapefile with the new points (new geometry and same properties as input file)
-    dataset = arcpy.InsertCursor(output_fl_split)
-
-    for l in split_lines_col.filter():
-        row = dataset.newRow()
-        mline = arcpy.Array()
-        for line_part in l['geometry']['coordinates']:
-            array = arcpy.Array()
-            for p in line_part:
-                point.X = p[0]
-                point.Y = p[1]
-                array.add(point)
-
-            mline.add(array)
-
-        row.Shape = mline
-
-        for field in fields_line:
-            if field.editable and field.baseName.lower() not in ['shape', 'fid', 'objectid']:
-                row.setValue(field.name, l['properties'].get(field.name, None))
-
-        row.setValue(id_field, str(l['properties'].get(id_field, None)))
-        row.setValue('tot_leng', l['properties'].get('tot_leng', None))
-
-        dataset.insertRow(row)
-    add_result_to_display(output_fl_split, output_name_split)
 
 # Add results to display
 add_result_to_display(output_fl, output_name)
