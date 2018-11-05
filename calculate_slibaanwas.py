c4d4828fbc1dd16933f9206dd3fe87630d8ec79e
elma13 <elma@tenner.nl>
2018-11-05 15:12:20 +0100

    Update calculate_slibaanwas.py

--- calculate_slibaanwas.py
+++ calculate_slibaanwas.py
@@ -8,7 +8,7 @@ from utils.addresulttodisplay import add_result_to_display
 from gistools.tools.combine_peilingen import combine_peilingen, convert_to_metfile, results_dict_to_csv
 from gistools.utils.conversion_tools import get_float
 
-from gistools.tools.calculate_slibaanwas_tool import GetProfielMiddelpunt
+from gistools.tools.calculate_slibaanwas_tool import GetProfielMiddelpunt, Createbuffer, GetSlibaanwas, CalcSlibaanwas
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'external'))
 
@@ -30,4 +30,7 @@ input_inpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekene
 input_uitpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VP_02_points.shp'
 output_folder = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas'
 
-test = GetProfielMiddelpunt(input_inpeil, input_uitpeil)
+point_col_in, point_col_uit, point_col_mid_in, point_col_mid_uit, profiel_namen_in, profiel_namen_uit = GetProfielMiddelpunt(input_inpeil, input_uitpeil)
+buffer_mid_in = Createbuffer(point_col_mid_in)
+
+Test = GetSlibaanwas(point_col_in,point_col_uit,point_col_mid_uit,buffer_mid_in)
