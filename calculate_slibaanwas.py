be5331c0366ad7e78fd35f12bbe4f87cf80f5094
elma13 <elma@tenner.nl>
2018-11-07 15:16:39 +0100

    Verder aan tool gewerkt

--- calculate_slibaanwas.py
+++ calculate_slibaanwas.py
@@ -8,7 +8,7 @@ from utils.addresulttodisplay import add_result_to_display
 from gistools.tools.combine_peilingen import combine_peilingen, convert_to_metfile, results_dict_to_csv
 from gistools.utils.conversion_tools import get_float
 
-from gistools.tools.calculate_slibaanwas_tool import GetProfielMiddelpunt, Createbuffer, GetSlibaanwas, CalcSlibaanwas
+from gistools.tools.calculate_slibaanwas_tool import GetProfielMiddelpunt, Createbuffer, GetSlibaanwas, CalcSlibaanwas, WriteListtoCollection
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'external'))
 
@@ -26,11 +26,20 @@ log.setLevel(logging.INFO)
 # inpuit_uitpeil = arcpy.GetParameterAsText(1)
 # output_folder = arcpy.GetParameterAsText(2)
 
-input_inpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VP_02_points.shp'
-input_uitpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VP_02_points.shp'
+#input_inpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VP_02_points.shp'
+#input_uitpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VP_02_points.shp'
+
+
+input_inpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VDH_IBA080_201800216_points.shp'
+input_uitpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VDH_IBA080_20170215_points.shp'
+
 output_folder = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas'
 
 point_col_in, point_col_uit, point_col_mid_in, point_col_mid_uit, profiel_namen_in, profiel_namen_uit = GetProfielMiddelpunt(input_inpeil, input_uitpeil)
 buffer_mid_in = Createbuffer(point_col_mid_in)
 
-Test = GetSlibaanwas(point_col_in,point_col_uit,point_col_mid_uit,buffer_mid_in)
+in_uit_combi, info_list = GetSlibaanwas(point_col_in,point_col_uit,point_col_mid_uit,buffer_mid_in)
+
+WriteListtoCollection(output_folder,in_uit_combi, info_list)
+print slibaanwas
+print box_lengte
