479e24ffb978aae51914727732ae4e199aa711aa
elma13 <elma@tenner.nl>
2018-11-30 16:04:13 +0100

    Update calculate_slibaanwas.py

--- calculate_slibaanwas.py
+++ calculate_slibaanwas.py
@@ -4,11 +4,13 @@ import logging
 import arcpy
 from utils.arcgis_logging import setup_logging
 import arcpy
+import time
 from utils.addresulttodisplay import add_result_to_display
 from gistools.tools.combine_peilingen import combine_peilingen, convert_to_metfile, results_dict_to_csv
 from gistools.utils.conversion_tools import get_float
 
-from gistools.tools.calculate_slibaanwas_tool import GetProfielMiddelpunt, Createbuffer, GetSlibaanwas, CalcSlibaanwas, WriteListtoCollection
+from gistools.tools.calculate_slibaanwas_tool import GetProfielMiddelpunt, Createbuffer,\
+    GetSlibaanwas, WriteListtoCollection
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'external'))
 
@@ -31,15 +33,23 @@ log.setLevel(logging.INFO)
 
 
 input_inpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VDH_IBA080_201800216_points.shp'
+#input_inpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VDH_IBA080_20170215_kortlang_points.shp'
+
 input_uitpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\VDH_IBA080_20170215_points.shp'
 
-output_folder = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas'
+input_inpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\proj_slibaanwas_all\\20180718_IBA_meetjaar_2_points.shp'
+input_uitpeil = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\proj_slibaanwas_all\\20181108_IBA_meetjaar_1_points.shp'
+output_folder = 'C:\Users\elma\Documents\GitHub\Test_data_werking_tools\\berekenen_slibaanwas\\proj_slibaanwas_all'
 
-point_col_in, point_col_uit, point_col_mid_in, point_col_mid_uit, profiel_namen_in, profiel_namen_uit = GetProfielMiddelpunt(input_inpeil, input_uitpeil)
+# Aanroepen functies voor berekening slib:
+point_col_in, point_col_uit, point_col_mid_in, point_col_mid_uit, profiel_namen_in, profiel_namen_uit = \
+    GetProfielMiddelpunt(input_inpeil, input_uitpeil)
 buffer_mid_in = Createbuffer(point_col_mid_in)
-
+t = time.time()
 in_uit_combi, info_list = GetSlibaanwas(point_col_in,point_col_uit,point_col_mid_uit,buffer_mid_in)
+elapsed = time.time() - t
 
-WriteListtoCollection(output_folder,in_uit_combi, info_list)
-print slibaanwas
-print box_lengte
+print('TIJD: ', elapsed)
+
+# Wegschrijven gegevens in shapefile
+WriteListtoCollection(output_folder, in_uit_combi, info_list)
