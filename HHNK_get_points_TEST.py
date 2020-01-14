cf9f83fbcd8e444ee0181699d091577c3e67dbc0
tomtengtijhuis <54531648+tomtengtijhuis@users.noreply.github.com>
2020-01-14 14:19:12 +0100

    hhnk update
    update

--- HHNK_get_points_TEST.py
+++ HHNK_get_points_TEST.py
@@ -2,7 +2,7 @@
 import arcpy
 import fiona
 from shapely import affinity
-from shapely.geometry import Point, LineString, shape, MultiPolygon, Polygon, MultiLineString
+from shapely.geometry import Point, LineString, shape, MultiPolygon, Polygon, MultiLineString, mapping
 from gistools.tools.connect_start_end_points import get_points_on_line
 from gistools.tools.dwp_tools import get_haakselijnen_on_points_on_line
 from matplotlib.collections import PatchCollection
@@ -45,13 +45,15 @@ def closest(lst, K):
 #TI19340_Te_peilen_Waterlopen_Tuitjenhorn.shp
 #waterloop_ingewikkeld.shp
 #TI19340_Te_peilen_Waterlopen_Waarland.shp
-waterloop_lines = fiona.open("C:/Users/tom/_Python_projects/HHNK_profielen_intekenen/test/TI19340_Te_peilen_Waterlopen_Waarland.shp")
+#Waterlijnen_totaal.shp
+waterloop_lines = fiona.open("C:/Users/tom/_Python_projects/HHNK_profielen_intekenen/test/waterlijn_clip_singlepart.shp")
 
 #waterdelen openen (importeren)
 #waterdeel_selection_test.shp
 #waterdeel_ingewikkeld.shp
 #waterdeel_waarland.shp
-shapes = MultiPolygon([shape(pol['geometry']) for pol in fiona.open("C:/Users/tom/_Python_projects/HHNK_profielen_intekenen/test/waterdeel_waarland.shp")])
+#waterdeel_HHNK.shp
+shapes = MultiPolygon([shape(pol['geometry']) for pol in fiona.open("C:/Users/tom/_Python_projects/HHNK_profielen_intekenen/test/waterdeel_HHNK_dissolved.shp")])
 
 plt.figure(figsize=(20,20))
 
@@ -62,8 +64,11 @@ fixed_distance_profile = 50.0
 #looproutes openen (importeren)
 #waterloop_ingewikkeld.shp
 #TI19340_Te_peilen_Waterlopen_Tuitjenhorn.shp
-# #I19340_Te_peilen_Waterlopen_Waarland.shp
-for waterloop_index, pol in enumerate(fiona.open("C:/Users/tom/_Python_projects/HHNK_profielen_intekenen/test/TI19340_Te_peilen_Waterlopen_Waarland.shp")):
+#TI19340_Te_peilen_Waterlopen_Waarland.shp
+#Waterlijnen_totaal.shp
+point_list = []
+counter = 1
+for waterloop_index, pol in enumerate(fiona.open("C:/Users/tom/_Python_projects/HHNK_profielen_intekenen/test/waterlijn_clip_singlepart.shp")):
     test_col = MemCollection(geometry_type='LineString')
     records = []
     records.append(pol)
@@ -125,28 +130,45 @@ for waterloop_index, pol in enumerate(fiona.open("C:/Users/tom/_Python_projects/
                 if not haakse_lijn.centroid.xy:
                     raise Exception("no centroid point for intersection")
 
+                # if waterloop_index == 371:
+                #     print(counter)
+                #     counter += 1
+
                 # searches two intersects closest to the centroid point
                 intersect_1 = []
                 intersect_2 = []
                 for intersection in intersections_x:
+                    if len(intersections_x[0]) < 3:
+                        pass
                     if all_same(intersection):
                         intersect_1.append(intersection[0])
                         intersect_2.append(intersection[0])
                     else:
+                        count = 0
                         for i, x_coordinate in enumerate(intersection):
-                            if round(x_coordinate, 2) == round(haakse_lijn.centroid.xy[0][0], 2):
-                                intersect_1.append(intersection[i - 1])
-                                intersect_2.append(intersection[i + 1])
+                            if round(x_coordinate, 3) == round(haakse_lijn.centroid.xy[0][0], 3):
+                                count += 1
+                                if count == 1 and len(intersection) != (i + 1):
+                                    intersect_1.append(intersection[i - 1])
+                                    intersect_2.append(intersection[i + 1])
 
+                # if counter == 26:
+                #     print("here")
                 for intersection in intersections_y:
-                    if all_same(intersection):
+                    if len(intersections_y[0]) < 3:
+                        pass
+                    elif all_same(intersection):
                         intersect_1.append(intersection[0])
                         intersect_2.append(intersection[0])
                     else:
+                        count = 0
                         for i, y_coordinate in enumerate(intersection):
-                            if round(y_coordinate, 2) == round(haakse_lijn.centroid.xy[1][0], 2):
-                                intersect_1.append(intersection[i - 1])
-                                intersect_2.append(intersection[i + 1])
+                            if round(y_coordinate, 3) == round(haakse_lijn.centroid.xy[1][0], 3):
+                                count += 1
+                                if count == 1 and len(intersection) != (i + 1):
+                                    intersect_1.append(intersection[i - 1])
+                                    intersect_2.append(intersection[i + 1])
+
 
                 # calculate distance between intersect points
                 try:
@@ -175,9 +197,11 @@ for waterloop_index, pol in enumerate(fiona.open("C:/Users/tom/_Python_projects/
             idx_list = []
             while i < (len(distance_list)):
                 if i == 0:
-                    slice = distance_list[i:int(i+profile_distance_transformed) / 2]
+                    slice = distance_list[i:int(i+profile_distance_transformed)]
                 else:
                     slice = distance_list[i:int(i+profile_distance_transformed)]
+                    if len(slice) == int(profile_distance_transformed):
+                        slice = distance_list[i+10:(int(i + profile_distance_transformed)-10)]
                 mean_distance = sum(slice)/len(slice)
                 closest_point = closest(slice, mean_distance)
                 idx = distance_list.index(closest_point)
@@ -190,12 +214,9 @@ for waterloop_index, pol in enumerate(fiona.open("C:/Users/tom/_Python_projects/
                     idx = 25
                     idx_list.append(idx)
                 elif i == 0 and len(distance_list) < 50:
-                    idx = len(distance_list) / 2
+                    idx = len(slice) / 2
                     idx_list.append(idx)
-                elif idx < 10 and len(distance_list) > (10 + idx): #if the best intersect of the line is within the first 15 meters:
-                    idx = idx + 10
-                    idx_list.append(idx) # add 10 meter
-                elif len(distance_list)- idx < 10 and len(distance_list) > (idx - 10): # if best intersect is 10 meters or shorter from end of line
+                elif len(distance_list) - idx < 10 and len(distance_list) > (idx - 10):  # if best intersect is 10 meters or shorter from end of line
                     idx = idx - 10
                     idx_list.append(idx)
                 else:
@@ -207,18 +228,28 @@ for waterloop_index, pol in enumerate(fiona.open("C:/Users/tom/_Python_projects/
 
             point_list_x = []
             point_list_y = []
+            haakse_lijnen_final = []
             for item in idx_list:
                 if idx_list > 0:
+                    haakse_lijnen_final.append(LineString(haakse_lijnen[item]['geometry']['coordinates']))
                     point_list_x.append(point_col[item]['geometry']['coordinates'][0])
                     point_list_y.append(point_col[item]['geometry']['coordinates'][1])
-            plt.scatter(point_list_x, point_list_y, c='r', zorder=15, s=50)
+                    point_list.append(Point(point_col[item]['geometry']['coordinates']))
+
+            #plot points
+            # plt.scatter(point_list_x, point_list_y, c='r', zorder=15, s=50)
+
+            # plot haakse lijnen of correct locations
+            # for i in range(len(haakse_lijnen_final)):
+            #     x,y = haakse_lijnen_final[i].xy
+            #     plt.plot(x,y, c="r")
 
             # calculate BGT waterdelen
-            for shape in shapes:
-                x,y = shape.exterior.xy
-
-              #plot BGT waterdelen
-                plt.plot(x,y, c='b', zorder=5)
+            # for shape in shapes:
+            #     x,y = shape.exterior.xy
+            #
+            #   #plot BGT waterdelen
+            #     plt.plot(x,y, c='b', zorder=5)
 
                 # x,y = shape.interiors[0].xy
                 #
@@ -228,15 +259,30 @@ for waterloop_index, pol in enumerate(fiona.open("C:/Users/tom/_Python_projects/
 
 
             # plot waterlooplijnen
-            for line in waterloop_lines:
-                line_list_x = []
-                line_list_y = []
-                for i in range(len(line['geometry']['coordinates'])):
-                    line_list_x.append(line['geometry']['coordinates'][i][0])
-                    line_list_y.append(line['geometry']['coordinates'][i][1])
-                    plt.plot(line_list_x, line_list_y, zorder=10)
+#             for line in waterloop_lines:
+#                 line_list_x = []
+#                 line_list_y = []
+#                 for i in range(len(line['geometry']['coordinates'])):
+#                     line_list_x.append(line['geometry']['coordinates'][i][0])
+#                     line_list_y.append(line['geometry']['coordinates'][i][1])
+#                     plt.plot(line_list_x, line_list_y, zorder=10)
+#
+# plt.show()
 
-plt.show()
+
+schema = {
+    'geometry': 'Point',
+    'properties': {'id': 'int'},
+}
+
+# Write a new Shapefile
+with fiona.open('C:/Users/tom/_Python_projects/HHNK_profielen_intekenen/test/totaal_punten_clip_dissolve.shp', 'w', 'ESRI Shapefile', schema, crs_wkt=waterloop_lines.crs_wkt) as c:
+    ## If there are multiple geometries, put the "for" loop here
+    for e, point in enumerate(point_list):
+        c.write({
+            'geometry': mapping(point),
+            'properties': {'id': e},
+        })
 
 ### OVERIG
 
